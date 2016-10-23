bb2fe77b8e35b9763f6ce82718f7892b5f97fb13
andrew <atom1543@yandex.ru>
2016-10-23 12:31:30 +0300

    Added app custom_user. When you want to create a user, you must create manually custom_user and manually link them.

---
+++ project/custom_user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.CharField(max_length=100)
+
+
